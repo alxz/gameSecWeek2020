9d8820264482f1872921d6fa3a200dff832c9bac
alxz <azap_ad@yahoo.com>
2020-09-18 21:37:22 -0400

    fixed config file

--- lib/config.php
+++ lib/config.php
@@ -1,11 +1,6 @@
 <?php
-// define('DBHOST', 'localhost');
-// define('DBUSER', 'root');
-// define('DBPASS', 'root');
-// define('DBNAME', 'quizdb');
-define('DBHOST', '172.26.125.158');
-define('DBUSER', 'quizadm');
-define('DBPASS', 'Pt468YSJWXnsgGEm!');
+define('DBHOST', 'localhost');
+define('DBUSER', 'root');
+define('DBPASS', 'root');
 define('DBNAME', 'quizdb');
-//
 ?>
